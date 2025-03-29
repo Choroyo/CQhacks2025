43ad0c146f791ea46d7fc76525676cf8850100b7
barfa04 <fatimab4r@gmail.com>
2025-03-29 16:25:22 -0500

    save background chages

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def main():
     score = 0
 
     # Fondo de carretera dinamico
-    background_image = pygame.image.load("images\plainBackground.jpg").convert()
+    background_image = pygame.image.load("images/forestb.png").convert()
     background_y = 0 #Position initial
 
     difficulty = 0
